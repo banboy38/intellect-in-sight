76923074209ef61e9f53c3e417a8a17b48c7b21b
Anirban Aditya Halder <71653026+banboy38@users.noreply.github.com>
2024-02-10 11:34:25 +0530

    Update Query.js

--- components/Query.js
+++ components/Query.js
@@ -6,7 +6,7 @@ export default function Query({text}){
 
         // Query Chatbox
         <div className="flex gap-3 justify-end items-center ">
-            <div className="rounded min-w-[50%] p-3 bg-blue-800 text-white shadow-md break-words hover:scale-[1.01] duration-300 ease-in-out">{text}</div>
+            <div className="rounded min-w-[30%] p-3 bg-blue-800 text-white shadow-md break-words hover:scale-[1.01] duration-300 ease-in-out">{text}</div>
             <div className="w-[2rem]"><FaUserCircle size={30} color="rgb(30 64 175)"/></div>
         </div>
     )
