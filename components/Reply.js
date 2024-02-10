16e5db0f0093860f6f83047d6009b72d387221ea
Anirban Aditya Halder <71653026+banboy38@users.noreply.github.com>
2024-02-10 11:34:36 +0530

    Update Reply.js

--- components/Reply.js
+++ components/Reply.js
@@ -6,7 +6,7 @@ export default function Reply({text}){
         // Reply Chatbox
         <div className="flex gap-3 justify-start items-center">
             <div className="w-[2rem]"><FaRobot size={30} color="rgb(30 64 175)"/></div>
-            <div className="rounded min-w-[50%] p-3 bg-white shadow-md break-words hover:scale-[1.01] duration-300 ease-in-out">{text}</div>
+            <div className="rounded min-w-[30%] p-3 bg-white shadow-md break-words hover:scale-[1.01] duration-300 ease-in-out">{text}</div>
         </div>
     )
 }
