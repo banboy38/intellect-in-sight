a19669e315c31034a9a85e870836e186cf8e1785
Anirban Aditya Halder <71653026+banboy38@users.noreply.github.com>
2024-02-11 00:59:31 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Main from "@/components/Main";
+import SideBar from "@/components/SideBar";
+import { useState } from "react";
+
+export default function Home() {
+  const [toggle, settoggle] = useState(1)
+  const [usecase, setusecase] = useState("Usecase 1")
+  const [conversation, setConversation] = useState([["bot","Hello! I'm your Bot!"]])
+
+  return (
+    <div className="bg-gray-100 flex justify-center items-center">
+      <SideBar setConversation={setConversation} setusecase={setusecase} settoggle={settoggle} toggle={toggle}/>
+      <Main conversation={conversation} setConversation={setConversation} usecase={usecase} toggle = {toggle}/>
+
+    </div>
+  );
+}
