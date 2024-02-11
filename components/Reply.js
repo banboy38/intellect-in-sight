dc8e1c2091cebeab7ece613792842ffe2261b6c5
Anirban Aditya Halder <71653026+banboy38@users.noreply.github.com>
2024-02-11 16:39:57 +0530

    Update Reply.js

--- components/Reply.js
+++ components/Reply.js
@@ -2,17 +2,19 @@ import { useEffect, useState } from "react";
 import { FaRobot } from "react-icons/fa";
 
 export default function Reply({text}){
-
+    
     const [loadText, setloadText] = useState("")
-
-    useEffect(async () => {
-        
-        let s = ""
-        for(let i = 0; i<text.length; i++){
-            s += text[i]
-            setloadText(s)
+    
+    async function stepType() {
+        let s = "";
+        for (let i = 0; i < text.length; i++) {
+            s += text[i];
+            setloadText(s);
             await new Promise(resolve => setTimeout(resolve, 50));
         }
+    }
+    useEffect(() => {        
+        stepType();
     }, [])
     
     
@@ -24,4 +26,5 @@ export default function Reply({text}){
             <div className="rounded min-w-[30%] p-3 bg-white shadow-md break-words hover:scale-[1.01] duration-300 ease-in-out">{loadText}</div>
         </div>
     )
+
 }
